f96211427d3b1966cd6db6247edae904dde462e3
Damion Werner <damion.werner@gmail.com>
2023-09-24 18:04:08 +0200

    fix fibonacci and partition example

--- examples/fibonacci.py
+++ examples/fibonacci.py
@@ -4,5 +4,5 @@ You can even use fluentiter to make a very neat fibonacci one-liner ヽ(•‿�
 from fluentiter import iterator
 
 if __name__ == "__main__":
-    for e in iterator(range(10)).scan(0, lambda s, x: (x, s + x)):
+    for e in iterator(range(10)).scan((0, 0), lambda s, x: ((s[1], sum(s) or x), sum(s) or x)):
         print(e)
